8f459f999e0cdeeadee45a2e26e61bd89a4ff8d6
NikitaAndreevich7 <programmist-97@mail.ru>
2019-03-21 10:37:13 +0300

    Адаптивная верстка 100%. >Функционал

---
+++ js/main.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function(){
+        $('.tfs-box').hover(
+        
+         function(){ $(this).addClass('tarif')},
+        function(){ $(this).removeClass('tarif') }
+ );
+
+
+});//end ready
+
